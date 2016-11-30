cc81c51184a46087f1fa8ded9fff763de31d571b
Justin Moulton <moultonjust@gmail.com>
2016-11-30 18:30:24 -0500

    Add weight logic

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -30,18 +30,18 @@ export const getNextTurn = (board, turnCount) => {
 
     const checkVertical = (board, turn, player) => {
       board.forEach(cols => {
-        let lastPlayer = player;
-        let seqCount = 1;
+        let lastPlayer = "";
+        let seqCount = 0;
         const arr = cols.filter(x => x !== undefined);
         for (let index = 0; index < arr.length; index++) {
-          if (seqCount === 3 && arr[index + 1] === undefined) {
-            turn = {possible: true, arr: cols, col: cols[0].col};
-          }
-
-          if (lastPlayer === player) {
+          if ((lastPlayer === player && arr[index].turn === player) || lastPlayer === "") {
             seqCount++;
           } else {
             seqCount = 1;
+          }
+
+          if (seqCount === 3 && arr[index + 1] === undefined) {
+            turn = {possible: true, arr: cols, col: cols[0].col};
           }
 
           lastPlayer = arr[index].turn;
@@ -68,10 +68,28 @@ export const getNextTurn = (board, turnCount) => {
   };
 
   const getNextTurn = (board, turn) => {
+    let leftWeight = 0;
+    let rightWeight = 0;
+
+    for (let itemIndex = 0; itemIndex < board.length - 4; itemIndex++) {
+      const arr = board[itemIndex].filter(x => x !== undefined);
+      leftWeight += arr.length;
+    }
+
+    console.log(leftWeight);
+    for (let itemIndex = board.length - 1; itemIndex > 3; itemIndex--) {
+      const arr = board[itemIndex].filter(x => x !== undefined);
+      rightWeight += arr.length;
+    }
     for (let itemIndex = 0; itemIndex < board.length; itemIndex++) {
       const arr = board[itemIndex].filter(x => x !== undefined);
-      if (arr.length > 0 && arr[arr.length - 1].turn === "cpu") {
-        const item = itemIndex + 1 < board.length ? board[itemIndex + 1] : board[itemIndex - 1];
+      if (leftWeight > rightWeight || leftWeight === rightWeight) {
+        if (arr.length > 0 && arr[arr.length - 1].turn === "cpu") {
+          const item = itemIndex + 1 < board.length ? board[itemIndex + 1] : board[itemIndex - 1];
+          turn = {arr: item, col: arr[0].col};
+        }
+      } else if (arr.length > 0 && arr[arr.length - 1].turn === "cpu") {
+        const item = itemIndex - 1 > 0 ? board[itemIndex - 1] : board[itemIndex + 1];
         turn = {arr: item, col: arr[0].col};
       }
     }
@@ -89,8 +107,6 @@ export const getNextTurn = (board, turnCount) => {
   } else {
     const winGame = addFourthToken(board, turn, "cpu");
     const blockPlayer = addFourthToken(board, turn, "player");
-    console.log(winGame);
-    console.log(blockPlayer);
     if (winGame.possible === true) {
       turn = {arr: winGame.arr, col: winGame.col};
     } else if (blockPlayer.possible === true) {
